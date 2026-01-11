b9fd75bf8f4c18df4cd44bd0b883b5b51ff9a439
Dhani Dwi Septian Bangun <ddsbg27@gmail.com>
2026-01-11 19:29:51 +0700

    uhjj

---
+++ static/js/data.js
@@ -0,0 +1,105 @@
+const portfolioItems = [
+    {
+        id: 1,
+        title: "Ethereal Moments",
+        category: "Portrait",
+        cover: "static/assets/1/img-1.jpg",
+        images: [
+            "static/assets/1/img-1.jpg",
+            "static/assets/1/img-2.jpg",
+            "static/assets/1/img-3.jpg",
+            "static/assets/1/img-4.jpg",
+            "static/assets/1/img-5.jpg"
+        ]
+    },
+    {
+        id: 2,
+        title: "Urban Soul",
+        category: "Street",
+        cover: "static/assets/2/img-4.jpg",
+        images: [
+            "static/assets/2/img-1.jpg",
+            "static/assets/2/img-2.jpg",
+            "static/assets/2/img-3.jpg",
+            "static/assets/2/img-4.jpg",
+            "static/assets/2/img-5.jpg",
+            "static/assets/2/img-6.jpg"
+        ]
+    },
+    {
+        id: 3,
+        title: "Golden Hour",
+        category: "Nature",
+        cover: "static/assets/3/img-1.jpg",
+        images: [
+            "static/assets/3/img-1.jpg",
+            "static/assets/3/img-2.jpg",
+            "static/assets/3/img-3.jpg",
+            "static/assets/3/img-4.jpg"
+        ]
+    },
+    {
+        id: 4,
+        title: "Intimate Vows",
+        category: "Wedding",
+        cover: "static/assets/4/img-2.jpg",
+        images: [
+            "static/assets/4/img-1.jpg",
+            "static/assets/4/img-2.jpg",
+            "static/assets/4/img-3.jpg",
+            "static/assets/4/img-4.jpg",
+            "static/assets/4/img-5.jpg"
+        ]
+    },
+    {
+        id: 5,
+        title: "City Lights",
+        category: "Night",
+        cover: "static/assets/5/img-3.jpg",
+        images: [
+            "static/assets/5/img-1.jpg",
+            "static/assets/5/img-2.jpg",
+            "static/assets/5/img-3.jpg",
+            "static/assets/5/img-4.jpg"
+        ]
+    },
+    {
+        id: 6,
+        title: "Neon Dreams",
+        category: "Abstract",
+        cover: "static/assets/6/img-3.jpg",
+        images: [
+            "static/assets/6/img-1.jpg",
+            "static/assets/6/img-2.jpg",
+            "static/assets/6/img-3.jpg",
+            "static/assets/6/img-4.jpg"
+        ]
+    },
+    {
+        id: 7,
+        title: "The Celebration",
+        category: "Wedding",
+        cover: "static/assets/7/img-1.jpg",
+        images: [
+            "static/assets/7/img-1.jpg",
+            "static/assets/7/img-2.jpg",
+            "static/assets/7/img-3.jpg",
+            "static/assets/7/img-4.jpg",
+            "static/assets/7/img-5.jpg"
+        ]
+    },
+    {
+        id: 8,
+        title: "Midnight Flower",
+        category: "Nature",
+        cover: "static/assets/8/img-3.jpg",
+        images: [
+            "static/assets/8/img-1.jpg",
+            "static/assets/8/img-2.jpg",
+            "static/assets/8/img-3.jpg",
+            "static/assets/8/img-4.jpg",
+            "static/assets/8/img-5.jpg",
+            "static/assets/8/img-6.jpg"
+        ]
+    }
+];
